3f8abda1e9d7c5da74cac7bf519617c550c05b68
veera-17 <veeragokulraj@testpress.in>
2024-10-03 12:59:02 +0530

    Admin page Customized

--- catlog/admin.py
+++ catlog/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
 from .models import Book, BookInstance, Genre, Author, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    
+class BooksInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')    
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')    
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
--- catlog/models.py
+++ catlog/models.py
@@ -49,8 +49,8 @@ class Book(models.Model):
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book")
     
-    language = models.ManyToManyField(
-        'Language',on_delete=models.PROTECT, help_text="Select a Language for this book")
+    language = models.ForeignKey('Language', on_delete=models.PROTECT, help_text="Select a language for this book")
+
 
     def __str__(self):
         """String for representing the Model object."""
@@ -59,6 +59,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 
 
 class BookInstance(models.Model):
