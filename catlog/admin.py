2c9f68d325bc747616ce59461fff302a53f07df2
veera-17 <veeragokulraj@testpress.in>
2024-10-03 15:30:18 +0530

    Home page setup completed

--- catlog/admin.py
+++ catlog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Book, BookInstance, Genre, Author, Language
 
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
     
 class BooksInline(admin.TabularInline):
     model = Book
@@ -11,6 +9,10 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
+    
+    
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
     
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
--- catlog/views.py
+++ catlog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genre = Genre.objects.all().count()
+    num_book_contains_c = Book.objects.filter(title__icontains='c').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genre' : num_genre,
+        'num_book_contains_c' : num_book_contains_c
+    } 
+    
+    return render(request, 'index.html', context=context)
